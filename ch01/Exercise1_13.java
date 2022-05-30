c7103f57c770d0d05cd83c48312084fd45cc69e5
sleong110 <sleong110@gmail.com>
2022-05-30 14:27:22 +0800

    Commit Exercise1_13.java

---
+++ ch01/Exercise1_13.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class Exercise1_13 {
+    public static void main(String[] args) {
+        double x = (44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1);
+        double y = (3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1);
+        System.out.println("Value x: ");
+        System.out.println(x);
+        System.out.println("Value y: ");
+        System.out.println(y);
+    }
+}
