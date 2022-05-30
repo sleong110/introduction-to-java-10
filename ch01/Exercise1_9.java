79bc3ab42efac26111de9dd8be1351c79edd799e
sleong110 <sleong110@gmail.com>
2022-05-30 13:57:00 +0800

    Commit Exercise1_9.java

---
+++ ch01/Exercise1_9.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class Exercise1_9 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+        double perimeter = width * height;
+        System.out.println("Perimeter of a rectangle: ");
+        System.out.println(perimeter);
+    }
+}
