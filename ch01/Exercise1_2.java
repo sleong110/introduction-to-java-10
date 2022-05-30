8e1cc7e4b75d96418f13bc854cf209f22f104661
sleong110 <sleong110@gmail.com>
2022-05-30 10:44:46 +0800

    Commit Exercise1_2.java

---
+++ ch01/Exercise1_2.java
@@ -0,0 +1,14 @@
+package ch01;
+
+/**
+ * Write a program displays Welcome to Java five times.
+ */
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
