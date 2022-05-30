fd2d8ca42624b3483e880077b8578595d9ed5350
sleong110 <sleong110@gmail.com>
2022-05-30 10:40:23 +0800

    Commit Exercise1.1.java

---
+++ ch01/Exercise1_1.java
@@ -0,0 +1,13 @@
+package ch01;
+
+/**
+ * (Display three messages) Write a program that displays 
+ * Welcome to Java, Welcome to Computer Science, and Programming is fun.
+ */
+public class Exercise1_1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Wecome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+}
