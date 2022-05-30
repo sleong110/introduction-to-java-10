68b67a2279aacd7a2f60302c3ea59978de31435d
sleong110 <sleong110@gmail.com>
2022-05-30 14:01:01 +0800

    Commit Exercise1_10.java

---
+++ ch01/Exercise1_10.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class Exercise1_10 {
+    public static void main(String[] args) {
+        double distance = 14.0 / 1.6;
+        double totalTime = 45.5 / 60;
+        double averageSpeed = distance / totalTime;
+        System.out.println("Average speed in miles per hour: ");
+        System.out.println(averageSpeed);
+    }
+}
--- ch01/Exercise1_9.java
+++ ch01/Exercise1_9.java
@@ -4,7 +4,7 @@ public class Exercise1_9 {
     public static void main(String[] args) {
         double width = 4.5;
         double height = 7.9;
-        double perimeter = width * height;
+        double perimet      er = width * height;
         System.out.println("Perimeter of a rectangle: ");
         System.out.println(perimeter);
     }
