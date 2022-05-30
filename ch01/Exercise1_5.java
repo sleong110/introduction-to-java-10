fe4fc8030e7b071a6fc989703290ba4b867aba71
sleong110 <sleong110@gmail.com>
2022-05-30 11:10:23 +0800

    Commit Exercise1_5.java

---
+++ ch01/Exercise1_5.java
@@ -0,0 +1,11 @@
+package ch01;
+
+/**
+ * Write a program that displays the result of a computation.
+ */
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
