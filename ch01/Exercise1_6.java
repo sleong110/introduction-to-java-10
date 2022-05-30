7683850865568e6a96d5da378d52d05ec9c8037a
sleong110 <sleong110@gmail.com>
2022-05-30 11:16:27 +0800

    Commit Exercise1_6.java

---
+++ ch01/Exercise1_6.java
@@ -0,0 +1,10 @@
+package ch01;
+
+/**
+ * Write a program that displays the result of series computation.
+ */
+public class Exercise1_6 {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
