163d594cc4f4a00ed19a4d375b7881f46df1d6d7
sleong110 <sleong110@gmail.com>
2022-05-30 13:53:29 +0800

    Commit Exercise1_8.java

---
+++ ch01/Exercise1_8.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class Exercise1_8 {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        double r = 5.5;
+        System.out.println("perimeter: ");
+        System.out.println(2 * r * pi);
+        System.out.println("area: ");
+        System.out.println(r * r * pi);
+    }
+}
