00bb640d399aa3a253d70fbe0dbbf8f66e8ede34
sleong110 <sleong110@gmail.com>
2022-06-02 22:31:19 +0800

    Commit Exercise2_1.java

---
+++ ch02/Exercise2_1.java
@@ -0,0 +1,15 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter  a degree in Celsius: ");
+        double celsius = input.nextDouble();
+
+        double fahrenheit = ( 9.0 / 5 ) * celsius + 32;
+        System.out.println(celsius + " Celsius is " + fahrenheit + " Fahrenheit");
+    }
+}
