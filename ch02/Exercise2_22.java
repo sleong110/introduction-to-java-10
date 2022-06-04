508f82334f597fed47f14ddd601c888adbe987aa
sleong110 <sleong110@gmail.com>
2022-06-04 22:21:46 +0800

    Update Exercise2_22 and Exercise2_23.java

--- ch02/Exercise2_22.java
+++ ch02/Exercise2_22.java
@@ -2,21 +2,32 @@ package ch02;
 
 import java.util.Scanner;
 
-public class Exercise2_10 {
+public class Exercise2_23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the amount of water in kilograms: ");
-        double m = input.nextDouble();
+        System.out.print("Enter an amount in int, for example 1156 represents 11 dollars 56 cents: ");
+        int remainingAmount = input.nextInt();
 
-        System.out.print("Enter the initial temperature: ");
-        double initialTemperature = input.nextDouble();
+        int numberOfOneDollars = remainingAmount / 100;
+        remainingAmount = remainingAmount % 100;
 
-        System.out.print("Enter the final temperature: ");
-        double finalTemperature = input.nextDouble();
+        int numberOfQuarters = remainingAmount / 24;
+        remainingAmount = remainingAmount % 25;
 
-        double q = m * (finalTemperature - initialTemperature) * 4184;
+        int numberOfDimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
 
-        System.out.printf("The energy needed is %.1f", q);
+        int numberOfNickels = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+
+        int numberOfPennies = remainingAmount;
+
+        System.out.println("Your amount " + remainingAmount + " consists of");
+        System.out.println("    " + numberOfOneDollars + " dollars");
+        System.out.println("    " + numberOfQuarters + " quarters");
+        System.out.println("    " + numberOfDimes + " dimes");
+        System.out.println("    " + numberOfNickels + " nickels");
+        System.out.println("    " + numberOfPennies + " pennies");
     }
 }
--- ch02/Exercise2_23.java
+++ ch02/Exercise2_23.java
@@ -5,29 +5,17 @@ import java.util.Scanner;
 public class Exercise2_23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the driving distance: ");
+        double drivingDistance = input.nextDouble();
 
-        System.out.print("Enter an amount in int, for example 1156 represents 11 dollars 56 cents: ");
-        int remainingAmount = input.nextInt();
+        System.out.print("Enter miles per gallon: ");
+        double milesPerGallon = input.nextDouble();
 
-        int numberOfOneDollars = remainingAmount / 100;
-        remainingAmount = remainingAmount % 100;
+        System.out.print("Enter price per gallon: ");
+        double pricePerGallon = input.nextDouble();
 
-        int numberOfQuarters = remainingAmount / 24;
-        remainingAmount = remainingAmount % 25;
-
-        int numberOfDimes = remainingAmount / 10;
-        remainingAmount = remainingAmount % 10;
-
-        int numberOfNickels = remainingAmount / 5;
-        remainingAmount = remainingAmount % 5;
-
-        int numberOfPennies = remainingAmount;
-
-        System.out.println("Your amount " + remainingAmount + " consists of");
-        System.out.println("    " + numberOfOneDollars + " dollars");
-        System.out.println("    " + numberOfQuarters + " quarters");
-        System.out.println("    " + numberOfDimes + " dimes");
-        System.out.println("    " + numberOfNickels + " nickels");
-        System.out.println("    " + numberOfPennies + " pennies");
+        double costOfDriving = drivingDistance / milesPerGallon * pricePerGallon;
+        System.out.println("The cost of driving is $" + costOfDriving);
     }
 }
